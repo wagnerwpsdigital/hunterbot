c7d5d11461bcbc0b4cf4cdc6d937c9b2ab948f4b
wagnerwpsdigital <145234712+wagnerwpsdigital@users.noreply.github.com>
2025-04-21 23:02:20 -0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -2,7 +2,7 @@ from supabase import create_client, Client
 
 # 🔐 Substitua pelos dados reais do seu projeto Supabase
 SUPABASE_URL = "https://fdwljwzievxxaanpfgvb.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZkd2xqd3ppZXZ4eGFhbnBmZ3ZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUyODE2NDksImV4cCI6MjA2MDg1NzY0OX0.znkl3rxvaMuqdyMSpNQcni847d3fqXcXzA-t4HNIMo4"
+SUPABASE_KEY = "GdHK0TUtEm7bQVKS"
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 def login(email, senha):
